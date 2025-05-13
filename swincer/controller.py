105a6701a1e7114dfcae3b4eb1782f1ed9fe233c
Youllou <juless.barbier@gmail.com>
2025-05-13 14:02:26 -0400

    git push create user so i dont have issues with distant connection

--- swincer/controller.py
+++ swincer/controller.py
@@ -22,15 +22,15 @@ class SwinceSession:
 
     def connect_to_guild_database(self):
         # Get MySQL connection details from environment variables
-        username = os.getenv("MYSQL_USER")
-        password = os.getenv("MYSQL_PASSWORD")
+        username = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
         root_password = os.getenv("MYSQL_ROOT_PASSWORD")
         host = os.getenv("MYSQL_HOST")
         port = os.getenv("MYSQL_PORT", 3306)
 
         # Create a new engine for the guild's database
         mysql_engine = create_engine(
-            f"mysql+pymysql://root:{root_password}@db/"
+            f"mysql+pymysql://{username}:{password}@db/"
         )
         with mysql_engine.connect() as connection:
             connection.execute(text(f"CREATE DATABASE IF NOT EXISTS guild_{self.db_name}"))  # create db
